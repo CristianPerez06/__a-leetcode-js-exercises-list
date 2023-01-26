24e97aa5bf4544c97a80e18418f14d77e8590c56
Cristian Perez <cristian.ap84@gmail.com>
2023-01-26 14:06:26 -0300

    Updated exercise 026.

--- 026 - remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 026 - remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -1,26 +1,40 @@
 // Time Complexity O(n)
 // Space Complexity O(1)
 
+// const removeDuplicates = (nums) => {
+//   let pointer = 0
+
+//   for (let index = 1; index < nums.length; index++) {
+//     const currentIndexNum = nums[index]
+//     const currentPointerNum = nums[pointer]
+
+//     if (currentIndexNum != currentPointerNum) {
+//       // Add 1 to Pointer
+//       pointer++
+//       // Overwrite duplicated value with the next one valid
+//       // Example:
+//       // Previous state = [...7,7,8...]
+//       // Next     state = [...7,7,8...]
+//       //  -> nums[pointer] = 7 (the duplicated one) and nums[index] = 8
+//       nums[pointer] = nums[index]
+//     }
+//   }
+
+//   return nums.length ? pointer + 1 : 0
+// }
+
 const removeDuplicates = (nums) => {
-  let pointer = 0
+  const countNonDuplicateds = (accumulator, currentValue, index, nums) => {
+    const nextValue = nums[index + 1]
 
-  for (let index = 1; index < nums.length; index++) {
-    const currentIndexNum = nums[index]
-    const currentPointerNum = nums[pointer]
-
-    if (currentIndexNum != currentPointerNum) {
-      // Add 1 to Pointer
-      pointer++
-      // Overwrite duplicated value with the next one valid
-      // Example:
-      // Previous state = [...7,7,8...]
-      // Next     state = [...7,7,8...]
-      //  -> nums[pointer] = 7 (the duplicated one) and nums[index] = 8
-      nums[pointer] = nums[index]
-    }
+    return nextValue !== undefined && currentValue !== nextValue
+      ? accumulator + 1
+      : accumulator
   }
 
-  return nums.length ? pointer + 1 : 0
+  const total = nums.reduce(countNonDuplicateds, 0)
+
+  return nums.length ? total + 1 : 0
 }
 
 const list = [1, 2, 3, 3, 4, 5, 6, 7, 7, 8]
